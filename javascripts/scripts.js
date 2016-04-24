4f4df58e2e508b06a675903854c9c7b0f730be4b
Panigrahi, Swaraj (US - Bengaluru) <swapanigrahi@deloitte.com>
2016-04-24 14:21:46 +0530

    1. Menu added 2. bootstrap js added.

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,5 +1,24 @@
 jQuery('a[href^="#"]').click(function(e) {
-    jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top}, 1000);
-    return false;
+    var hash = $(this).attr('href');
+    jQuery('html,body').animate({
+        scrollTop: jQuery(this.hash).offset().top
+    }, 1000, function() {
+        var title = "Swaraj Panigrahi | ";
+        var crntSection = hash.split("-")[1];
+        crntSection = crntSection.charAt(0).toUpperCase() + crntSection.substr(1);
+        $("title").text(title + crntSection);
+    });
     e.preventDefault();
+    return false;
+});
+
+$(document).ready(function() {
+  // $(".navbar-toggle").on("click", function() {
+  //     $(this).toggleClass("active");
+  //     // $(".navbar-collapse").toggleClass("collapse");
+  // });
+
+  $(".navbar-nav > li").on("click", function(){
+    $(this).toggleClass("active");
+  });
 });
